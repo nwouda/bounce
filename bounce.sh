3957eecc6b5d179cfe3eacbee486fe0312c36bd0
Nathan Wouda <nathan.wouda@local-isp.nl>
2015-03-14 15:52:53 +0100

    Put strings between quotes

--- bounce.sh
+++ bounce.sh
@@ -5,50 +5,49 @@ lines=$(($lines-1))
 cols=$(tput cols)
 x=0
 y=0
-cursorz="😄 😃 😀 😊 ☺ 😉 😍 😘 😚 😗 😙 😜 😝 😛 😳 😁 😔 😌 😒 😞 😣 😢 😂 😭 😪 😥 😰 😅 😓 😩 😫 😨 😱 😠 😡 😤 😖 😆 😋 😷 😎 😴 😵 😲 😟 😦 😧 😈 👿 😮 😬 😐 😕 😯 😶 😇 😏 😑 👲 👳 👮 👷 💂 👶 👦 👧 👨 👩 👴 👵 👱 👼 👸"
-IFS=' '
+cursorz="😄 😃 😀 😊 😉 😍 😘 😚 😗 😙 😜 😝 😛 😳 😁 😔 😌 😒 😞 😣 😢 😂 😭 😪 😥 😰 😅 😓 😩 😫 😨 😱 😠 😡 😤 😖 😆 😋 😷 😎 😴 😵 😲 😟 😦 😧 😈 👿 😮 😬 😐 😕 😯 😶 😇 😏 😑 👲 👳 👮 👷 💂 👶 👦 👧 👨 👩 👴 👵 👱 👼 👸"
 cursors=($cursorz)
 num_cursors=${#cursors[*]}
 cursor=${cursors[$((RANDOM%num_cursors))]}
-xmode=inc
-ymode=inc
+xmode="inc"
+ymode="inc"
 while :; do
-  tput cup	$y $x
+  tput cup $y $x
   printf $cursor
-  if [ $xmode == inc ]; then
+  if [ $xmode == "inc" ]; then
     if [ $x -lt $cols ]; then
       x=$(($x+1))
     else
-      xmode=dec
+      xmode="dec"
       x=$(($x-1))
       cursor=${cursors[$((RANDOM%num_cursors))]}
       printf 
     fi
-  elif [ $xmode == dec ]; then
+  elif [ $xmode == "dec" ]; then
     if [ $x -gt 0 ]; then
       x=$(($x-1))
     else
-      xmode=inc
+      xmode="inc"
       x=$(($x+1))
       cursor=${cursors[$((RANDOM%num_cursors))]}
       printf 
     fi
   fi
 
-  if [ $ymode == inc ]; then
+  if [ $ymode == "inc" ]; then
     if [ $y -lt $lines ]; then
       y=$(($y+1))
     else
-      ymode=dec
+      ymode="dec"
       y=$(($y-1))
       cursor=${cursors[$((RANDOM%num_cursors))]}
       printf 
     fi
-  elif [ $ymode == dec ]; then
+  elif [ $ymode == "dec" ]; then
     if [ $y -gt 0 ]; then
       y=$(($y-1))
     else
-      ymode=inc
+      ymode="inc"
       y=$(($y+1))
       cursor=${cursors[$((RANDOM%num_cursors))]}
       printf 
